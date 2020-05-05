0215b3b8b4add2928cbce1557b272c13b3c1c8c9
Karl Manalo <karlmanalo@comcast.net>
2020-05-05 15:19:38 -0600

    Adds comments to IQR

--- my_lambdata/iqr.py
+++ my_lambdata/iqr.py
@@ -3,9 +3,13 @@
 import numpy
 
 def iqr(X):
+    # Finds midpoint of Q1
     Q1 = numpy.percentile(X, 25, interpolation='midpoint')
+
+    # Finds midpoint of Q3
     Q3 = numpy.percentile(X, 75, interpolation='midpoint')
 
+    # Calculates IQR
     iqr = Q3 - Q1
 
     return iqr
